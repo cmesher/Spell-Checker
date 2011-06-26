5e5b386306dbfc4268e243f955cb1fe87d590f0d
Shyam Shankar <syamsankar91@gmail.com>
2011-06-26 22:30:30 +0300

    I wrote about the License aspects of this software.
    I wrote a dedication to Ian Clarke and released the code in GPL license.
    
    I edited .gitignore so git will ignore all backup files.

--- using bloomfilter/bloomfilter12.java
+++ using bloomfilter/bloomfilter12.java
@@ -12,6 +12,8 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+ 
+ // This is inspired by the work of Ian Clarke and Magnus Skjegstad . Thank you guys! :)
 
 
 
